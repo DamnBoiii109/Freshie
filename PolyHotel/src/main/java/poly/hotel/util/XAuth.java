eaf410d3ebb50130045df52a1be1108555886cfd
Willno0 <jackerror2019@gmail.com>
2025-07-14 14:36:02 +0700

    Add files via upload

---
+++ PolyHotel/src/main/java/poly/hotel/util/XAuth.java
@@ -0,0 +1,14 @@
+package poly.hotel.util;
+
+import poly.hotel.entity.User;
+
+public class XAuth {
+    public static User user = User.builder()
+            .username("user1@gmail.com")
+            .password("123")
+            .enabled(true)
+            .manager(true)
+            .fullname("Nguyễn Văn Tèo")
+            .photo("trump.png")
+            .build();
+}

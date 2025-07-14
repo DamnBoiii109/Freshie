cbe32bfeb3f35e64cee5e19dc5e644523b5612ec
vanbao211128 <baonvts01995@gmail.com>
2025-07-14 21:15:35 +0700

    Add files via upload

---
+++ Freshie-main/Freshie-main/PolyHotel/src/main/java/poly/hotel/util/XAuth.java
@@ -0,0 +1,14 @@
+package poly.hotel.util;
+
+
+
+public class XAuth {
+    public static User user = User.builder()
+            .username("user1@gmail.com")
+            .password("123")
+            .enabled(true)
+            .manager(true)
+            .fullname("Nguyễn Văn Tèo")
+            .photo("trump.png")
+            .build();
+}

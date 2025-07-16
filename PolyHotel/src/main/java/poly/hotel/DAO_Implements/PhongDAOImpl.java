0bab815da1cae72a5680ab913bf49588f3dd57fd
phatntts <Phatntts02039@gmail.com>
2025-07-16 15:13:34 +0700

    Update PhongDAOImpl.java

--- PolyHotel/src/main/java/poly/hotel/DAO_Implements/PhongDAOImpl.java
+++ PolyHotel/src/main/java/poly/hotel/DAO_Implements/PhongDAOImpl.java
@@ -4,23 +4,23 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 import poly.cafe.dao.CardDAO;
-import poly.cafe.entity.Card;
+import poly.cafe.entity.Phong;
 import poly.cafe.util.XJdbc;
 
 /**
  *
  * @author hp user
  */
-public class CardDAOImpl implements CardDAO {
+public class CardDAOImpl implements PhongDAO {
 
-    private final String createSql = "INSERT INTO Cards (Id, Status) VALUES (?, ?)";
-    private final String updateSql = "UPDATE Card SET Status = ? WHERE Id = ?";
-    private final String deleteSql = "DELETE FROM Cards WHERE Id = ?";
-    private final String findAllSql = "SELECT * FROM Cards";
-    private final String findByIdSql = "SELECT * FROM Cards WHERE Id = ?";
+    private final String createSql = "INSERT INTO Phong (Id, Status) VALUES (?, ?)";
+    private final String updateSql = "UPDATE Phong SET Status = ? WHERE Id = ?";
+    private final String deleteSql = "DELETE FROM Phong WHERE Id = ?";
+    private final String findAllSql = "SELECT * FROM Phong";
+    private final String findByIdSql = "SELECT * FROM Phong WHERE Id = ?";
 
     @Override
-    public Card create(Card entity) {
+    public Phong create(Phong entity) {
         try {
             XJdbc.executeUpdate(createSql, entity.getId(), entity.getStatus());
             return entity; // Trả về entity vừa được thêm
@@ -31,12 +31,12 @@ public class CardDAOImpl implements CardDAO {
     }
 
     @Override
-    public void update(Card entity) {
+    public void update(Phong entity) {
         try {
             XJdbc.executeUpdate(updateSql, entity.getStatus(), entity.getId());
         } catch (Exception e) {
             e.printStackTrace();
-            throw new RuntimeException("Error updating card: " + e.getMessage());
+            throw new RuntimeException("Error updating Phong: " + e.getMessage());
         }
     }
 
@@ -46,18 +46,18 @@ public class CardDAOImpl implements CardDAO {
             XJdbc.executeUpdate(deleteSql, id);
         } catch (Exception e) {
             e.printStackTrace();
-            throw new RuntimeException("Error deleting card: " + e.getMessage());
+            throw new RuntimeException("Error deleting Phong: " + e.getMessage());
         }
     }
 
     @Override
-    public List<Card> findAll() {
-        List<Card> list = new ArrayList<>();
+    public List<Phong> findAll() {
+        List<Phong> list = new ArrayList<>();
         try (ResultSet rs = XJdbc.executeQuery(findAllSql)) {
             while (rs.next()) {
-                Card card = new Card();
-                card.setId(rs.getInt("Id"));
-                card.setStatus(rs.getInt("Status"));
+                Phong Phong = new Phong();
+                Phong.setId(rs.getInt("Id"));
+                Phong.setStatus(rs.getInt("Status"));
                 list.add(card);
             }
         } catch (Exception e) {
@@ -68,13 +68,13 @@ public class CardDAOImpl implements CardDAO {
     }
 
     @Override
-    public Card findById(Integer id) {
+    public Phong findById(Integer id) {
         try (ResultSet rs = XJdbc.executeQuery(findByIdSql, id)) {
             if (rs.next()) {
-                Card card = new Card();
-                card.setId(rs.getInt("Id"));
-                card.setStatus(rs.getInt("Status"));
-                return card;
+                Phong Phong = new Phong();
+                Phong.setId(rs.getInt("Id"));
+                Phong.setStatus(rs.getInt("Status"));
+                return Phong;
             }
         } catch (Exception e) {
             e.printStackTrace();

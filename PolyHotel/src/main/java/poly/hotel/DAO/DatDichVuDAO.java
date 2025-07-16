c8f5869d7d6cea227d71024cbaa4eeaaed8722f8
Tin02044 <tinvdts002044@gmail.com>
2025-07-16 14:50:49 +0700

    Update DatDichVuDAO.java

--- PolyHotel/src/main/java/poly/hotel/DAO/DatDichVuDAO.java
+++ PolyHotel/src/main/java/poly/hotel/DAO/DatDichVuDAO.java
@@ -1,1 +1,14 @@
-//con cac ba may bao
+
+package Poly.Hotel.dao;
+
+import java.util.List;
+import poly.cafe.entity.Drink;
+
+/**
+ *
+ * @author truon
+ */
+public interface DrinkDAO extends CrudDAO<Drink, String>{ 
+    List<Drink> findByCategoryId(String categoryId); 
+} 
+

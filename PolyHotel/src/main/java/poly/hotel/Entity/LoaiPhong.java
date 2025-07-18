72aa5d36b59e348e0d8270b129ca3acfce8fa018
trung325 <quoctrungp24@gmail.com>
2025-07-18 13:05:32 +0700

    Update LoaiPhong.java

--- PolyHotel/src/main/java/poly/hotel/Entity/LoaiPhong.java
+++ PolyHotel/src/main/java/poly/hotel/Entity/LoaiPhong.java
@@ -1,1 +1,20 @@
 
+package Poly.Hotel.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+/**
+ *
+ * @author truon
+ */
+@NoArgsConstructor 
+@AllArgsConstructor 
+@Builder 
+@Data 
+public class Category { 
+    private String id; 
+    private String name; 
+} 
+

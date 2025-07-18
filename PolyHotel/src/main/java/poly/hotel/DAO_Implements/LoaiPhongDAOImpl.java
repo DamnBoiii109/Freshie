258e3f234eb913191c57123ca056a06e8d546c98
trung325 <quoctrungp24@gmail.com>
2025-07-18 13:21:09 +0700

    Update LoaiPhongDAOImpl.java

--- PolyHotel/src/main/java/poly/hotel/DAO_Implements/LoaiPhongDAOImpl.java
+++ PolyHotel/src/main/java/poly/hotel/DAO_Implements/LoaiPhongDAOImpl.java
@@ -1,5 +1,5 @@
 
-package poly.hotel.DAO.Impl;
+package Poly.hotel.DAO.Impl;
 
 import java.util.List;
 import poly.cafe.dao.LoaiPhongDAO;
@@ -13,17 +13,17 @@ import poly.cafe.util.XQuery;
  */
 public class LoaiPhongDAOImpl implements LoaiPhongDAO { 
  
-    String createSql = "INSERT INTO Categories(Id, Name) VALUES(?, ?)"; 
-    String updateSql = "UPDATE Categories SET Name=? WHERE Id=?"; 
-    String deleteSql = "DELETE FROM Categories WHERE Id=?"; 
-    String findAllSql = "SELECT * FROM Categories"; 
-    String findByIdSql = "SELECT * FROM Categories WHERE Id=?"; 
+    String createSql = "INSERT INTO LoaiPhong(LoaiPhong, TenLoaiPhong, GiaPhong, GhiChu) VALUES(?,?)"; 
+    String updateSql = "UPDATE LoaiPhong SET TenLoaiPhong=? WHERE LoaiPhong=?"; 
+    String deleteSql = "DELETE FROM LoaiPhong WHERE LoaiPhong=?"; 
+    String findAllSql = "SELECT * FROM LoaiPhong"; 
+    String findByLoaiPhongSql = "SELECT * FROM LoaiPhong WHERE LoaiPhong=?"; 
  
     @Override 
     public LoaiPhong create(LoaiPhong entity) { 
         Object[] values = { 
-            entity.getId(), 
-            entity.getName() 
+            entity.getLoaiPhong(), 
+            entity.getTenLoaiPhong() 
         }; 
         XJdbc.executeUpdate(createSql, values); 
         return entity; 
@@ -31,21 +31,21 @@ public class LoaiPhongDAOImpl implements LoaiPhongDAO {
     @Override 
     public void update(LoaiPhong entity) { 
         Object[] values = { 
-            entity.getName(), 
-            entity.getId() 
+            entity.getTenLoaiPhong(), 
+            entity.getLoaiPhong() 
         }; 
         XJdbc.executeUpdate(updateSql, values); 
     } 
     @Override 
-    public void deleteById(String id) { 
-        XJdbc.executeUpdate(deleteSql, id); 
+    public void deleteByLoaiPhong(String LoaiPhong) { 
+        XJdbc.executeUpdate(deleteSql, LoaiPhong); 
     } 
     @Override 
     public List<LoaiPhong> findAll() { 
         return XQuery.getEntityList(LoaiPhong.class, findAllSql); 
     } 
     @Override 
-    public LoaiPhong findById(String id) { 
-        return XQuery.getSingleBean(LoaiPhong.class, findByIdSql, id); 
+    public LoaiPhong findByLoaiPhong(String LoaiPhong) { 
+        return XQuery.getSingleBean(LoaiPhong.class, findByLoaiPhongSql, LoaiPhong); 
     } 
 } 
